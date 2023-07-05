0a9ff8c2d527221e07797fa52ed56ac05b16536d
taofang <1664320691@qq.com>
2023-07-05 12:27:06 +0800

    nginx和hashhistory解决

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,18 @@ module.exports = defineConfig({
   // 关闭eslint语法验证
   lintOnSave: false,
   transpileDependencies: true,
+  /**
+     * 注意，nginx配置后，刷新可能会存在404问题，解决方案如下：
+     * 
+     * 第一种方案
+location /shifun {
+  alias   /var/www/html/shifun/;
+  index index.html;
+  try_files $uri $uri/ /shifun/index.html;
+}
+      第二种方案
+      通过hashhistory # 来解决
+     */
   publicPath: process.env.NODE_ENV === 'production' ? '/shifun/' : '/',
   devServer: {
     proxy: { //配置跨域  baseurl在 utils/request.js中修改‘/api’
